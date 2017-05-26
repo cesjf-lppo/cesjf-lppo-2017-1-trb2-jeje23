bd2ff35e50af7eacc0978e77bc20a617da469943
Jessica Barbosa <je_barbosaa@hotmail.com>
2017-05-25 22:52:41 -0300

    Criação do Servlet Lista Leitura

---
+++ src/java/br/cesjf/lppo/servlet/ListaLeituraServlet.java
@@ -0,0 +1,55 @@
+
+package br.cesjf.lppo.servlet;
+
+
+import br.cesjf.lppo.Leitura;
+import br.cesjf.lppo.dao.ColetaELeituraDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "ListaLeituraServlet", urlPatterns = {"/listaLeitura.html"})
+public class ListaLeituraServlet extends HttpServlet {
+
+   
+    
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+  /*      List<Leitura> leituras;
+        
+        try {
+            ColetaELeituraDAO dao = new ColetaELeituraDAO();
+            //leituras = dao.listAll();
+        } catch (Exception ex) {
+            Logger.getLogger(ListaColetaServlet.class.getName()).log(Level.SEVERE, null, ex);
+            leituras = new ArrayList<>();
+            request.setAttribute("mensagem", ex.getLocalizedMessage()); 
+        }
+        
+        
+        request.setAttribute("leituras" , //leituras);
+        request.getRequestDispatcher("/WEB-INF/lista-leitura.jsp").forward(request, response);*/
+        
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+   
+  
+}
